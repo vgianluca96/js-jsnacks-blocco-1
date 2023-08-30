7ecd6e0103121b5dcaf4ca8629bd2cc473c24f78
vgianluca96 <gianluca.vallese@hotmail.com>
2023-08-30 11:46:45 +0200

    snack 2 bonus

--- jssnack2/script.js
+++ jssnack2/script.js
@@ -5,6 +5,7 @@ L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga.
 */
 
+/*
 let word1 = prompt('Digita la prima parola');
 let word2 = prompt ('Digita la seconda parola');
 let resultMessage;
@@ -19,3 +20,29 @@ if (word1.length > word2.length) {
 
 console.log(resultMessage);
 alert(resultMessage);
+*/
+
+/*
+Bonus:
+rifarlo usando ciclo while
+*/
+
+
+let i = 0;
+let words = [];
+
+
+while (i <= 1) {
+
+    words[i] = prompt ('Digita una parola');
+    i++
+
+}
+
+if (words[0].length > words[1].length) {
+    console.log(words[0]);
+} else if (words[0].length < words[1].length) {
+    console.log(words[1]);
+} else {
+    console.log('parole lunghe uguali');
+}
