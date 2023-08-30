2d1ac7b50da7f9a9ece3cf8c26ac283c2488c5c1
vgianluca96 <gianluca.vallese@hotmail.com>
2023-08-30 12:14:47 +0200

    snack 3 bonus

--- jssnack3/script.js
+++ jssnack3/script.js
@@ -5,7 +5,7 @@ Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 */
 
-
+/*
 let numbersSum = 0;
 let ulElem = document.querySelector('ul');
 let liElem;
@@ -24,3 +24,26 @@ for (let i = 1; i<= 10; i++) {
 liElem = `<li>Somma numeri inseriti: ${numbersSum}</li>`;
 ulElem.insertAdjacentHTML('beforeend',liElem);
 console.log('Somma numeri inseriti: ' + numbersSum);
+*/
+
+/*
+Bonus:
+rifarlo usando ciclo while
+*/
+
+let numbersSum = 0;
+let i = 0;
+
+while (i < 10) {
+
+    let number = Number(prompt('Digita un numero'));
+
+    if (isNaN(number)) {
+        alert('Devi digitare un numero');
+    } else {
+        i++;
+        numbersSum +=number;
+    }
+}
+
+console.log(numbersSum);
