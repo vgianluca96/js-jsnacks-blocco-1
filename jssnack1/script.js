2717560b46ba98b30f1cd9137b23ac19d0a3d5a5
vgianluca96 <gianluca.vallese@hotmail.com>
2023-08-30 10:52:39 +0200

    snack 1 bonus
    rifatto con ciclo while

--- jssnack1/script.js
+++ jssnack1/script.js
@@ -5,6 +5,7 @@ L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.
 */
 
+/*
 let number1 = prompt('Inserisci il primo numero');
 let number2 = prompt ('Inserisci il secondo numero');
 let resultMessage;
@@ -19,3 +20,32 @@ if (Number(number1) > Number(number2)) {
 
 console.log(resultMessage);
 alert(resultMessage);
+*/
+
+
+/*
+Bonus:
+rifarlo usando ciclo while
+*/
+
+let numbers = [];
+let i = 1;
+
+while (i <= 2) {
+
+    numbers[i] = prompt('Inserisci un numero');
+
+    if (isNaN(numbers[i])) {
+        console.log('Devi digitare un numero');
+    } else {
+        i++
+    }
+}
+
+if (numbers[1]>numbers[2]) {
+    console.log(numbers[1]);
+} else if (numbers[1]>numbers[2]) {
+    console.log(numbers[2]);
+} else {
+    console.log('sono uguali');
+}
